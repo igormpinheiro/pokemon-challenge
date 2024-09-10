5a5bc9b0443e14cf2ed43a4d3a260a230ed77227
Igor Pinheiro <igordf@gmail.com>
2024-09-10 19:33:11 -0300

    chore: add serilog

--- src/PokeChallenge.API/Program.cs
+++ src/PokeChallenge.API/Program.cs
@@ -1,12 +1,21 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Host.UseSerilog((context, loggerConfig) =>
+    loggerConfig.ReadFrom.Configuration(context.Configuration));
+
 var app = builder.Build();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 app.UseHttpsRedirection();
+
 await app.RunAsync();
