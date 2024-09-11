d241450e3d5c9c165b2db01c1f7944d85df25ccf
Igor Pinheiro <igordf@gmail.com>
2024-09-11 16:59:51 -0300

    lint

--- src/PokeChallenge.API/Application/Abstractions/Messaging/ICommandHandler.cs
+++ src/PokeChallenge.API/Application/Abstractions/Messaging/ICommandHandler.cs
@@ -3,14 +3,12 @@ using MediatR;
 
 namespace PokeChallenge.API.Application.Abstractions.Messaging;
 
-public interface ICommandHandler<in TCommand>
-    : IRequestHandler<TCommand, IErrorOr>
+public interface ICommandHandler<in TCommand> : IRequestHandler<TCommand, IErrorOr>
     where TCommand : ICommand
 {
 }
 
-public interface ICommandHandler<in TCommand, TResponse>
-    : IRequestHandler<TCommand, ErrorOr<TResponse>>
+public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, ErrorOr<TResponse>>
     where TCommand : ICommand<TResponse>
 {
 }
